179998d3e2ee6d5a3e68014a99f83bb636144b3e
Andrew Henkel <search1230@gmail.com>
2022-12-08 14:38:04 -0500

    Modified fsdb.js to create directory of file

--- components/fsdb.js
+++ components/fsdb.js
@@ -1,8 +1,14 @@
-const fs = require("fs");
+const fs = require("fs")
+const path = require("path")
 
 var loadDatabase = (db_connection, schema = {}) => {
+    const dirname = path.dirname(db_connection)
+    if (!fs.existsSync(dirname)) {
+        fs.mkdirSync(dirname, { recursive: true })
+    }
+
     if (!fs.existsSync(db_connection)) {
-        fs.writeFileSync(db_connection, JSON.stringify(schema));
+        fs.writeFileSync(db_connection, JSON.stringify(schema))
     }
     const model = require(db_connection)
 
@@ -10,10 +16,10 @@ var loadDatabase = (db_connection, schema = {}) => {
         model: model,
         filename: db_connection,
         update: () => {
-            fs.writeFileSync(db_connection, JSON.stringify(model));
+            fs.writeFileSync(db_connection, JSON.stringify(model))
         }
     }
-    return db;
+    return db
 }
 
-module.exports = loadDatabase;
+module.exports = loadDatabase
